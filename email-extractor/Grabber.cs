cd11ff5f87390c8d4c6cc12fcfd4e45c0585f5a8
Christoph <christoph@rustige.me>
2023-07-27 22:22:45 +0200

    Initial Commit + Basic Implementation
    Build the basic implementation.
    The program is able to get E-Mail Addresses from a supplied URL, but might show duplicates.

---
+++ email-extractor/Grabber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace email_extractor
+{
+    public class Grabber
+    {
+        internal static void GrabCode(String url)
+        {
+            //Get Page Source Code
+            using (HttpClient client = new())
+            {
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string result = content.ReadAsStringAsync().Result;
+                        GetMails(result);
+                    }
+                }
+            }
+        }
+
+        internal static void GetMails(String sourceCode)
+        {
+            string emailPattern = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b";
+
+            // Use Regex.Matches to find all occurrences of email addresses in the input string
+            MatchCollection matches = Regex.Matches(sourceCode, emailPattern, RegexOptions.IgnoreCase);
+
+            if (matches.Count > 0)
+            {
+                Console.WriteLine("Email addresses found in the string:");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No email addresses found in the string.");
+            }
+        }
+    }
+}
---
+++ email-extractor/Program.cs
@@ -0,0 +1,9 @@
+﻿using email_extractor;
+string url = "";
+
+Console.WriteLine("Please enter the URL you want to extract from");
+url = Console.ReadLine();
+
+Console.WriteLine("Scanning " + url + " for email-addresses...");
+Grabber.GrabCode(url);
+
