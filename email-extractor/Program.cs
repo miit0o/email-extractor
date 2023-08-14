3353af86c5f67b736646dc49728684e42d561780
Christoph <christoph@rustige.me>
2023-08-15 01:25:46 +0200

    Minor spelling fixes

--- email-extractor/Program.cs
+++ email-extractor/Program.cs
@@ -4,7 +4,7 @@ string url = "";
 Console.WriteLine("Please enter the URL you want to extract from");
 url = Console.ReadLine();
 
-Console.WriteLine("Would you like to safe the extracted emails to a file? (Y/N)");
+Console.WriteLine("Would you like to save the extracted emails to a file? (Y/N)");
 string decision = Console.ReadLine();
 if (decision.Equals("Y") || decision.Equals("y"))
 {
